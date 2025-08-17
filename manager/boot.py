286305a670d21d787c6aec03161dc8e5ceb024d3
lhw <1466397749@qq.com>
2025-08-17 10:32:36 +0800

    test: test new decorators

--- manager/boot.py
+++ manager/boot.py
@@ -10,6 +10,7 @@ import os
 from constant import BASE_SKILL_PATH, INIT_FILE, BASE_PATH
 import sys
 
+import sys
 if os.path.dirname(BASE_PATH) not in sys.path:
     sys.path.append(os.path.dirname(BASE_PATH))
 from DeepEmbody.manager.eaios_decorators import package_init, mcp_start
